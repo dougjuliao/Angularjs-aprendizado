4d7e0490ab3f77a35ffae3e5577890777d817f2a
dougjuliao <douglas_juliao16@hotmail.com>
2014-11-06 16:36:25 -0200

    Adicionado seção 2 input

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 (function(){
 	var app = angular.module('geral',[]); // primeiro módulo - geral:'nome da aplicação', []:'dependencias';
+	/** CONTROLLER ADD TITULO ***/
 	app.controller('titulo',function(){
 		this.title = titPage;
 	});
@@ -7,4 +8,20 @@
 		nome: 'Aprendendo Angularjs',
 		desc: 'Iniciando aulas em angularjs abaixo encontra-se um menu com os exemplos que serão mostrados... clique :)'
 	};
+	/** CONTROLLER ADD CONTEUDO SECTION ***/
+	app.controller('contentSection',function(){
+		this.content = section;
+	});
+
+	var section = {
+		image: [
+			"img/1.png",
+			"img/2.png",
+			"img/3.png"
+		],
+		texto: function(textoInput){
+			return textoInput;
+		}
+	}
+
 })();
